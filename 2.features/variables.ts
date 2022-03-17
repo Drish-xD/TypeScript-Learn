65f8273aadcd270722bb3f8ca31883387bbf5721
Drish-xD <drish.xd@gmail.com>
2022-03-17 15:29:28 +0530

    variables

--- 2.features/variables.ts
+++ 2.features/variables.ts
@@ -52,3 +52,34 @@ const logNumber: (i: number) => void = (i: number) => {
 // Deteacts types of variable automatically
 
 const num2 = 5;
+
+// When To Use Type Annotations
+// 1) Function that returns the 'any' type
+
+const json = '{ "x": 10, "y": 20 }';
+const coordinates = JSON.parse(json);
+console.log(coordinates);
+
+// 2) When we decleare a variable in one line and init it later
+
+const colors2 = ["red", "green", "blue"];
+let foundWord: boolean = false;
+
+for (let i = 0; i < colors2.length; i++) {
+  if (colors2[i] === "blue") {
+    foundWord = true;
+  }
+  console.log(foundWord);
+}
+
+// 3) Variable whose type cannot be inferred correctly
+
+let numbers = [-1 - 5, 8];
+let numberAboveZero: boolean | number = false;
+
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] > 0) {
+    numberAboveZero = numbers[i];
+  }
+  console.log(numberAboveZero);
+}
