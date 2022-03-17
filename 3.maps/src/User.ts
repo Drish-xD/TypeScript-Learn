5c6b43842caef2181bb58d59f571328a29c5ef71
Drish-xD <drish.xd@gmail.com>
2022-03-18 01:04:51 +0530

    google maps

---
+++ 3.maps/src/User.ts
@@ -0,0 +1,17 @@
+import { faker } from "@faker-js/faker";
+
+export class User {
+  name: string;
+  location: {
+    lat: number;
+    lng: number;
+  };
+
+  constructor() {
+    this.name = faker.name.findName();
+    this.location = {
+      lat: parseFloat(faker.address.longitude()),
+      lng: parseFloat(faker.address.latitude()),
+    };
+  }
+}
