ccb4b43c76bb7325c7c74424d44dd7e61847bdce
Drish-xD <drish.xd@gmail.com>
2023-07-30 00:49:05 +0530

    inheritence

--- 4.sort/src/index.ts
+++ 4.sort/src/index.ts
@@ -2,13 +2,21 @@ import { CharactersCollection } from './CharactersCollection';
 import { LinkedList } from './LinkedList';
 import { NumberCollection } from './NumberCollection';
 
+// numbers array
 const numberCollection = new NumberCollection([2, 55, -5]);
+numberCollection.sort();
+console.log(numberCollection.data);
+
+// String
 const charCollection = new CharactersCollection('Xaayb');
+charCollection.sort();
+console.log(charCollection.data);
+
+// linked list
 const linkedList = new LinkedList();
-
-// linkedList.add(600);
-// linkedList.add(10);
-// linkedList.add(-6);
-// linkedList.add(50);
-
-// const sorter = new Sorter(linkedList);
+linkedList.add(600);
+linkedList.add(10);
+linkedList.add(-6);
+linkedList.add(50);
+linkedList.sort();
+linkedList.print();
